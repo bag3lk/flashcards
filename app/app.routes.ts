831bb66a586f33798b8c7fac23f52b39d71591cf
bag3lk <czemujalol@outlook.com>
2025-04-08 13:03:33 +0200

    co

--- app/app.routes.ts
+++ app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { fiszka1 } from './flashcard1/flashcard1.component';
+import { AppComponent } from './app.component';
 
 export const routes: Routes = [
-    {path: 'flashcard1', component: fiszka1}
+    {path: 'flashcard1', component: fiszka1},
+    {path: 'home', component: AppComponent}
 ];
