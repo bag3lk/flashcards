790e1421731450bfdda1142b4001d73090892568
bag3lk <czemujalol@outlook.com>
2025-04-15 10:58:46 +0200

    dodany scrollbar

--- app/flashcard3/flashcard3.component.ts
+++ app/flashcard3/flashcard3.component.ts
@@ -8,9 +8,10 @@ import { CommonModule} from '@angular/common';
   templateUrl: './flashcard3.component.html',
   styleUrl: './flashcard3.component.css'
 })
+
 export class Flashcard3Component {
   fiszka = [
-    {question: "Jaką objętość miały płuca", answer: "5dm³"},
+    {question: "Jaką objętość posiadają płuca", answer: "5dm³"},
     {question: "Jakie jest prawidłowe tętno człowieka", answer: "120/80"},
     {question: "Czym sie różni D/N/A od RNA?", answer: "RNA jest jedną nicią, DNA jest ciągłe po obu stornach"},
     {question: "Na jaki typ infrkcji trzeba brać antybiotyk", answer: "Bakteryjny"},
@@ -19,21 +20,18 @@ export class Flashcard3Component {
     {question: "Czy rak może mieć włosy, zęby a nawet i oczy?", answer: "Tak"},
     {question: "W jakim okresie czasu SIDS może zabić dziecko?", answer: "Rok od urodzenia"},
     {question: "Przez jaką drogę się rozprzestrzenia sepsa", answer: "Krwionośną"},
-    {question: "Aby urodzić dziecko, jak bardzo kobieta musi być rozszerzona?", answer: "10cm"},
     {question: "Jaki kwas mamy w żołądku?", answer: "Kwas solny"},
     {question: "Jeśli twoi rodzice mają grupę krwi A, to jaką ty masz", answer: "A, albo 0"},
     {question: "Co to astygmatyzm?", answer: "Astygmatyzm to wada wzroku spowodowana nieregularnym kształtem rogówki lub soczewki oka."},
   ]
-    index = 0;
-    isVisible = false;
+  index = 0;
+  isVisible = false;
     
-    ans(){
-      this.isVisible = true;
-    }
-    
+  ans(){
+    this.isVisible = true;
+  }
   next(){
     this.index++;
     this.isVisible = false;
   }
-
 }
