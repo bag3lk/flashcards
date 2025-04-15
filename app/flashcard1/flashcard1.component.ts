7b215b56b9b01cb3e791921a01542fa074ef5cd9
bag3lk <czemujalol@outlook.com>
2025-04-15 08:45:21 +0200

    pokazywanie odpowiedzi i nastepna fiszka

--- app/flashcard1/flashcard1.component.ts
+++ app/flashcard1/flashcard1.component.ts
@@ -1,16 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-flashcard1',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './flashcard1.component.html',
   styleUrl: './flashcard1.component.css'
 })
 export class fiszka1 {
-  fiszka = {
-    "geografia": {question: "Co jest stolicą Kanady?", answer: "Ottawa"},
-    "chemia": {question: "Jaki jest symbol tlenu?", answer: "O"},
-    "angielski": {question:"Jak przetłumaczysz 'przedmiot' na angielski?", answer: "Object"},
+  fiszka = [
+    {question: "Co jest stolicą Kanady?", answer: "Ottawa"}, //0
+    {question: "Jaki jest symbol tlenu?", answer: "O"}, //1
+    {question:"Jak przetłumaczysz 'przedmiot' na angielski?", answer: "Object"} //2
+  ]
+    index = 0;
+    isVisible = false;
+    
+    ans() {
+      this.isVisible = true;
+    }
+    
+  next() {
+    this.index++;
+    this.isVisible = false;
   }
 }
